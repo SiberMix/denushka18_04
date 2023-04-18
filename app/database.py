ec52234c01e1ee4fc57e82a06a093a20fb1b5ecf
SiberMix <off_mi4ka@vk.com>
2023-04-18 17:56:02 +0700

    fx: fx config sqlalchemy

--- app/database.py
+++ app/database.py
@@ -3,9 +3,12 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+load_dotenv()
 
-
-engine = create_engine(os.environ['DB_URL'])
+engine = create_engine(
+    f"postgresql+psycopg2://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}"
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
